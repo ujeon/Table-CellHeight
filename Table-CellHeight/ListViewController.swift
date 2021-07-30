4667e3236f00b7539663080ebfcb127195577c52
ujeon_jang <wj.jang@newploy.net>
2021-07-30 11:17:05 +0900

    estimatedRowHeight와 automaticDimension을 사용하여 테이블 셀의 높이를 자동으로 조절하기

---
+++ Table-CellHeight/ListViewController.swift
@@ -0,0 +1,44 @@
+import UIKit
+
+class ListViewController: UITableViewController {
+    var list = [String]()
+    
+    @IBAction func add(_ sender: Any) {
+        let alert = UIAlertController(title: "목록 입력", message: "추가될 글을 작성해주세요", preferredStyle: .alert)
+        
+        alert.addTextField() { (tf) in
+            tf.placeholder = "내용을 입력하세요"
+        }
+        
+        let ok = UIAlertAction(title: "확인", style: .default) { (_) in
+            if let title = alert.textFields?[0].text {
+                self.list.append(title)
+                self.tableView.reloadData()
+            }
+        }
+        
+        let cancel = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+        
+        alert.addAction(ok)
+        alert.addAction(cancel)
+        
+        self.present(alert, animated: false)
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.list.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell") ?? UITableViewCell()
+        
+        cell.textLabel?.numberOfLines = 0
+        cell.textLabel?.text = list[indexPath.row]
+        return cell
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.tableView.estimatedRowHeight = 50
+        self.tableView.rowHeight = UITableView.automaticDimension
+    }
+}
